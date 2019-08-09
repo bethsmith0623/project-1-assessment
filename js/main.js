69265641dc1b16d0a823f900ae2bff0d5d7e64c6
Beth Smith <bethsmith0623@gamil.com>
2019-08-09 13:23:16 -0500

    adds hover

--- js/main.js
+++ js/main.js
@@ -14,7 +14,6 @@ let subtract = document.getElementById("subtract");
 add.addEventListener('click', addVal);
 subtract.addEventListener('click',subtractVal);
 
-
 /*----- functions -----*/
 initialize();
 

fe2fcd62cbf4fe70d6031ef32cae409f511a8d4f
Beth Smith <bethsmith0623@gamil.com>
2019-08-09 10:25:51 -0500

    sets up html & css

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+let input, count;
+
+/*----- cached element references -----*/
+let inputVal = document.getElementById("input");
+let countVal = document.getElementById("count");
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+init();
+
+function init() {
+  input = 0;
+  count = 1;
+  // render();
+};
+

972297d1d985947eabb47853de80aef36383afe4
Beth Smith <bethsmith0623@gamil.com>
2019-08-09 12:59:30 -0500

    adds button and display logic

--- js/main.js
+++ js/main.js
@@ -7,16 +7,34 @@ let input, count;
 /*----- cached element references -----*/
 let inputVal = document.getElementById("input");
 let countVal = document.getElementById("count");
+let add = document.getElementById("add");
+let subtract = document.getElementById("subtract");
 
 /*----- event listeners -----*/
+add.addEventListener('click', addVal);
+subtract.addEventListener('click',subtractVal);
 
 
 /*----- functions -----*/
-init();
+initialize();
 
-function init() {
-  input = 0;
-  count = 1;
-  // render();
+function initialize() {
+  input = 1;
+  count = 0;
+  render();
 };
 
+function render() {
+  document.getElementById("count").value = count;
+  document.getElementById("input").value = input;
+};
+
+function addVal() {
+  count += parseInt(inputVal.value);
+  render();
+};
+
+function subtractVal() {
+  count -= parseInt(inputVal.value);
+  render();
+};
